1845d19226193933df70c9aac29c94b9e1030df3
gabry9306 <gabrieledellaria34@gmail.com>
2017-05-22 17:52:20 +0200

    Add files via upload

--- flixnetbot.php
+++ flixnetbot.php
@@ -77,14 +77,13 @@ function GetSerie($chatId,$title)
 		$update_1 = json_decode($content_imdb, TRUE);
 	
 		$title_film = $update_1["0"]["show"]["name"];
-		$year_film = $update_1["data"]["0"]["year"];
 		$date_film = $update_1["0"]["show"]["premiered"];
-		$regista = $update_1["data"]["0"]["directors"]["0"]["name"];
-		$genere = $update_1["data"]["0"]["genres"]["0"];
-		$durata = $update_1["data"]["0"]["runtime"];
-		$premi = $update_1["data"]["0"]["awards"]["0"][""];
-		$trailer = $update_1["data"]["0"]["trailer"]["videoURL"];
-		$trama = $update_1["data"]["0"]["overview"];
+		$regista = $update_1["0"]["show"]["name"];
+		$genere = $update_1["0"]["show"]["genres"]["0"];
+		$durata = $update_1["0"]["show"]["runtime"];
+		$trailer = $update_1["0"]["show"]["name"];
+		$produttore = $update_1["0"]["show"]["webChannel"]["name"];
+		//$trama = $update_1["0"]["show"]["summary"];
 		$locandina = $update_1["0"]["show"]["image"]["original"];
 
 		/*$content_yadex = file_get_contents('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170520T205327Z.87b5aa9c5b1a21ee.578062198537d96ec63800ae1d0292d6911ee90f&text='.$trama.'&lang=it&options=1');
@@ -94,20 +93,19 @@ function GetSerie($chatId,$title)
 
 		for ($x = 0; $x <= 10; $x++) 
 		{
-			$premi[$x] = $update_1["data"]["movies"]["0"]["awards"][$x]["titlesAwards"]["0"]["titleAwardOutcome"];
-			$array_premi = implode(', ', $premi);
+			$genere[$x] = $update_1["0"]["show"]["genres"]["0"];
+			$array_genere = implode(', ', $genere);
 
-			if($premi[$x] == "")
+			if($genere[$x] == "")
 			{
 				break;
 			}  
 		} 
 
-		$message1 = "<b>Nome Serie:</b>%0A".$title_film."%0A %0A"."<b>Genere:</b>%0A".$genere."%0A %0A"."<b>Anno:</b>%0A".$year_film."%0A %0A"."<b>Regista:</b>%0A".$regista."%0A %0A"."<b>Data uscita:</b>%0A".$date_film."%0A %0A"."<b>Durata:</b>%0A".$durata."%0A %0A"."<b>Trama:</b>%0A".$trama."%0A %0A"."<b>Premi:</b>%0A".$array_premi;
+		$message1 = "<b>Nome Serie:</b>%0A".$title_film."%0A %0A"."<b>Genere:</b>%0A".$array_genere."<b>Data uscita 1° Episodio:</b>%0A".$date_film."%0A %0A"."<b>Durata Media Episodio:</b>%0A".$durata."%0A %0A"."<b>Produttore:</b>%0A".$produttore;
 
 		$tastiera_1 = '&reply_markup={"inline_keyboard":[[{"text":"TRAILER","url":"'.$trailer.'"}]]}';
-		$url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.$message1;//.$tastiera_1;
-		file_get_contents($url);
+		$url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.$message1.$tastiera_1;
 
 		$url = $GLOBALS[website].'/sendPhoto?chat_id='.$chatId.'&parse_mode=HTML&photo='.$locandina;
 		file_get_contents($url);
