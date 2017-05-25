c0dd092ecfb5093248e7f3664217a1ec8314ca12
gabry9306 <gabrieledellaria34@gmail.com>
2017-05-26 00:15:39 +0200

    Add files via upload

--- flixnetbot.php
+++ flixnetbot.php
@@ -186,9 +186,7 @@ function GetSerie($chatId,$title)
 
 			$message1 = "<b>Nome Serie:</b>%0A".$title_film."%0A %0A"."<b>Genere:</b>%0A".$genere."%0A %0A"."<b>Data uscita 1° Episodio:</b>%0A".$date_serie."%0A %0A"."<b>Durata Media Episodio:</b>%0A".$durata." min %0A %0A"."<b>Rating:</b>%0A".$rating."/10%0A %0A"."<b>Produttore:</b>%0A".$produttore."%0A %0A"."<b>Cast:</b>%0A".$casts."%0A %0A"."<b>Trama:</b>%0A".$trama."%0A %0A";
 
-			$message1 = trim($message1);
-			$remove = array(',','}','{',']','[',';',':','>','<','|',')','(','*','%','$','!','^','/');
-			$message1 = str_replace($remove, "", $message1);
+			$messagge1 = escape($messagge1);
  
 			if ( $trailer == "www.youtube.com/watch?v=/"){
 
@@ -268,6 +266,19 @@ function GetDiscoverSerie($chatId)
 				$url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.$message1;//.$tastiera_1;
 				file_get_contents($url);
 		
+}
+
+public function escape($string) {
+    $return = '';
+    for($i = 0; $i < strlen($string); ++$i) {
+        $char = $string[$i];
+        $ord = ord($char);
+        if($char !== "'" && $char !== "\"" && $char !== '\\' && $ord >= 32 && $ord <= 126)
+            $return .= $char;
+        else
+            $return .= '\\x' . dechex($ord);
+    }
+    return $return;
 }
 
 // ***************************************************************************************************************************** //
