efdbdc525496e4f820543a6ef883711582fed485
gabry9306 <gabrieledellaria34@gmail.com>
2017-05-26 00:06:43 +0200

    Add files via upload

--- flixnetbot.php
+++ flixnetbot.php
@@ -186,7 +186,7 @@ function GetSerie($chatId,$title)
 
 			$message1 = "<b>Nome Serie:</b>%0A".$title_film."%0A %0A"."<b>Genere:</b>%0A".$genere."%0A %0A"."<b>Data uscita 1° Episodio:</b>%0A".$date_serie."%0A %0A"."<b>Durata Media Episodio:</b>%0A".$durata." min %0A %0A"."<b>Rating:</b>%0A".$rating."/10%0A %0A"."<b>Produttore:</b>%0A".$produttore."%0A %0A"."<b>Cast:</b>%0A".$casts."%0A %0A"."<b>Trama:</b>%0A".$trama."%0A %0A";
 
-			$messagge1 = escape($message1);
+			$messagge1 = mres($message1);
  
 			if ( $trailer == "www.youtube.com/watch?v=/"){
 
@@ -268,18 +268,12 @@ function GetDiscoverSerie($chatId)
 		
 }
 
-// replace any non-ascii character with its hex code.
-function escape($value) {
-    $return = '';
-    for($i = 0; $i < strlen($value); ++$i) {
-        $char = $value[$i];
-        $ord = ord($char);
-        if($char !== "'" && $char !== "\"" && $char !== '\\' && $ord >= 32 && $ord <= 126)
-            $return .= $char;
-        else
-            $return .= '\\x' . dechex($ord);
-    }
-    return $return;
+function mres($value)
+{
+    $search = array("\\",  "\x00", "\n",  "\r",  "'",  '"', "\x1a");
+    $replace = array("\\\\","\\0","\\n", "\\r", "\'", '\"', "\\Z");
+
+    return str_replace($search, $replace, $value);
 }
 
 // ***************************************************************************************************************************** //
