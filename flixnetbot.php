b7be67d83390a8869c3277cc5da0c08d74e1ab3b
gabry9306 <gabrieledellaria34@gmail.com>
2017-05-26 00:13:29 +0200

    Add files via upload

--- flixnetbot.php
+++ flixnetbot.php
@@ -186,7 +186,9 @@ function GetSerie($chatId,$title)
 
 			$message1 = "<b>Nome Serie:</b>%0A".$title_film."%0A %0A"."<b>Genere:</b>%0A".$genere."%0A %0A"."<b>Data uscita 1° Episodio:</b>%0A".$date_serie."%0A %0A"."<b>Durata Media Episodio:</b>%0A".$durata." min %0A %0A"."<b>Rating:</b>%0A".$rating."/10%0A %0A"."<b>Produttore:</b>%0A".$produttore."%0A %0A"."<b>Cast:</b>%0A".$casts."%0A %0A"."<b>Trama:</b>%0A".$trama."%0A %0A";
 
-			$messagge1 = mysqli_escape_string($messagge1);
+			$message1 = trim($message1);
+			$remove = array(',','}','{',']','[',';',':','>','<','|',')','(','*','%','$','!','^','/');
+			$message1 = str_replace($remove, "", $message1);
  
 			if ( $trailer == "www.youtube.com/watch?v=/"){
 
