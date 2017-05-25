24ef73be9ac8235b9f1d6a45ed9080cba8c75e61
gabry9306 <gabrieledellaria34@gmail.com>
2017-05-26 00:23:26 +0200

    Add files via upload

--- flixnetbot.php
+++ flixnetbot.php
@@ -184,9 +184,7 @@ function GetSerie($chatId,$title)
 				$trama = "Non disponibile";
 			}
 
-			$message1 = "<b>Nome Serie:</b>%0A".$title_film."%0A %0A"."<b>Genere:</b>%0A".$genere."%0A %0A"."<b>Data uscita 1° Episodio:</b>%0A".$date_serie."%0A %0A"."<b>Durata Media Episodio:</b>%0A".$durata." min %0A %0A"."<b>Rating:</b>%0A".$rating."/10%0A %0A"."<b>Produttore:</b>%0A".$produttore."%0A %0A"."<b>Cast:</b>%0A".$casts."%0A %0A"."<b>Trama:</b>%0A".$trama."%0A %0A";
-
-			$messagge1 = escape($messagge1);
+			$message1 = "<b>Nome Serie:</b>%0A".$title_film."%0A %0A"."<b>Genere:</b>%0A".$genere."%0A %0A"."<b>Data uscita 1° Episodio:</b>%0A".$date_serie."%0A %0A"."<b>Durata Media Episodio:</b>%0A".$durata." min %0A %0A"."<b>Rating:</b>%0A".$rating."/10%0A %0A"."<b>Produttore:</b>%0A".$produttore."%0A %0A"."<b>Cast:</b>%0A".$casts."%0A %0A";//."<b>Trama:</b>%0A".$trama."%0A %0A";
  
 			if ( $trailer == "www.youtube.com/watch?v=/"){
 
@@ -266,19 +264,6 @@ function GetDiscoverSerie($chatId)
 				$url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.$message1;//.$tastiera_1;
 				file_get_contents($url);
 		
-}
-
-function escape($string) {
-    $return = '';
-    for($i = 0; $i < strlen($string); ++$i) {
-        $char = $string[$i];
-        $ord = ord($char);
-        if($char !== "'" && $char !== "\"" && $char !== '\\' && $ord >= 32 && $ord <= 126)
-            $return .= $char;
-        else
-            $return .= '\\x' . dechex($ord);
-    }
-    return $return;
 }
 
 // ***************************************************************************************************************************** //
