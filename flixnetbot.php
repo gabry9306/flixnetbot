f50861e164695772cc55cd010690f67a199812ff
gabry9306 <gabrieledellaria34@gmail.com>
2017-05-26 17:01:49 +0200

    Add files via upload

--- flixnetbot.php
+++ flixnetbot.php
@@ -190,6 +190,8 @@ function GetSerie($chatId,$title)
 
 			$message1 = "<b>Nome Serie:</b>%0A".$title_film."%0A %0A"."<b>Genere:</b>%0A".$genere."%0A %0A"."<b>Data uscita 1° Episodio:</b>%0A".$date_serie."%0A %0A"."<b>Durata Media Episodio:</b>%0A".$durata." min %0A %0A"."<b>Rating:</b>%0A".$rating."/10%0A %0A"."<b>Produttore:</b>%0A".$produttore."%0A %0A"."<b>Cast:</b>%0A".$casts;/*."<b>Trama:</b>%0A".$trama;*/
 
+			$message2 = "<b>Trama:</b>%0A".$trama;
+
 			if ( $trailer == "www.youtube.com/watch?v=/"){
 
 				$tastiera_1 = '&reply_markup={"inline_keyboard":[[{"text":"MAGGIORI INFO","url":"'.$link_imdb.'"}]]}';
@@ -201,6 +203,9 @@ function GetSerie($chatId,$title)
 
 				$tastiera_1 = '&reply_markup={"inline_keyboard":[[{"text":"MAGGIORI INFO","url":"'.$link_imdb.'"},{"text":"TRAILER","url":"'.$trailer.'"}]]}';
 				$url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.$message1.$tastiera_1;
+				file_get_contents($url);
+
+				$url = $GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.$message2;
 				file_get_contents($url);
 			}
 
