7c2bc759e15c9d4d5201212b46b0dcfd9a88c853
gabry9306 <gabrieledellaria34@gmail.com>
2017-05-26 11:16:18 +0200

    Add files via upload

--- flixnetbot.php
+++ flixnetbot.php
@@ -170,6 +170,8 @@ function GetSerie($chatId,$title)
 				$casts = "Non disponibile";
 			}
 
+			$trama = json_encode($trama);
+
 			// TRADUZIONE TRAMA 
 
 			$content_yadex = file_get_contents('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170520T205327Z.87b5aa9c5b1a21ee.578062198537d96ec63800ae1d0292d6911ee90f&text='.$trama.'&lang=it&options=1');
