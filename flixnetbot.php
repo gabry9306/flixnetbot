551469caa3972e9793f1338f5f9dfd5a7d0719c1
gabry9306 <gabrieledellaria34@gmail.com>
2017-05-27 15:37:55 +0200

    Add files via upload

--- flixnetbot.php
+++ flixnetbot.php
@@ -122,8 +122,8 @@ function GetSerie($chatId,$title)
 			/*$trama = str_replace_json(';',' ',$trama);
 			$trama = str_replace_json(':',' ',$trama);
 			$trama = str_replace_json(',',' ',$trama);
-			$trama = str_replace_json($apo,' ',$trama);*/
-			$trama = str_replace_json($slash,' ',$trama);
+			$trama = str_replace_json($apo,' ',$trama);
+			$trama = str_replace_json($slash,' ',$trama);*/
 
 			$trama = "<b>Trama:</b>%0A".$trama;
 
