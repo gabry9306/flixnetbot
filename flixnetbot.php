e1030f5cd0ba82155214a45b41efe89d1d0da7f9
gabry9306 <gabrieledellaria34@gmail.com>
2017-05-29 17:14:22 +0200

    Add files via upload

--- flixnetbot.php
+++ flixnetbot.php
@@ -222,9 +222,9 @@ function GetSerie($chatId,$title)
 			$cast_3 = $update_5["3"]["person"]["name"];
 			$cast_4 = $update_5["4"]["person"]["name"];
 
-			$casts = "Non disponibile";
+			$casts = "".$cast." - ".$cast_1." - ".$cast_2."%0A %0A".$cast_3." - ".$cast_4;
 
-			/*if ( $cast == "" )
+			if ( $cast == "" )
 			{
 				$casts = "".$cast_1."%0A".$cast_2."%0A".$cast_3."%0A".$cast_4."%0A";
 			}
@@ -247,9 +247,14 @@ function GetSerie($chatId,$title)
 			if ( $cast_4 == "" ){
 
 				$casts = "".$cast."%0A".$cast_1."%0A".$cast_2."%0A".$cast_3."%0A";
-			}*/
+			}
 
-			$message1 = "<b>Nome Serie:</b>%0A".$title_film."%0A %0A"."<b>Genere:</b>%0A".$genere."%0A %0A"."<b>Data uscita 1° Episodio:</b>%0A".$date_serie."%0A %0A"."<b>Durata Media Episodio:</b>%0A".$durata." min %0A %0A".$rating."<b>Produttore:</b>%0A".$produttore."%0A %0A"."<b>Cast:</b>%0A".$cast." - ".$cast_1." - ".$cast_2."%0A %0A".$cast_3." - ".$cast_4;
+			if ( $cast == "" & $cast_1 == "" & $cast_2 == "" & $cast_3 == "" & $cast_4 == "" ){
+
+				$casts = "Non disponibile";
+			}
+
+			$message1 = "<b>Nome Serie:</b>%0A".$title_film."%0A %0A"."<b>Genere:</b>%0A".$genere."%0A %0A"."<b>Data uscita 1° Episodio:</b>%0A".$date_serie."%0A %0A"."<b>Durata Media Episodio:</b>%0A".$durata." min %0A %0A".$rating."<b>Produttore:</b>%0A".$produttore."%0A %0A"."<b>Cast:</b>%0A".$casts;
 			$message2 = $trama;
 
 			if ( $trailer == "www.youtube.com/watch?v=/"){
