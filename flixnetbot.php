51db089a31a3d10d04991c5929b6cbbd4b30b24a
gabry9306 <gabrieledellaria34@gmail.com>
2017-05-29 18:57:52 +0200

    Add files via upload

--- flixnetbot.php
+++ flixnetbot.php
@@ -265,6 +265,26 @@ function GetSerie($chatId,$title)
 				$casts = "Non disponibile";
 			}
 
+			if ( $cast == "")
+			{
+				$casts = "".$cast_1."%0A".$cast_2;
+			}
+
+			if ( $cast_1 == "")
+			{
+				$casts = "".$cast."%0A".$cast_2;
+			}
+
+			if ( $cast_2 == "" ){
+
+				$casts = "".$cast."%0A".$cast_1;
+			}
+
+			if ( $cast == "" & $cast_1 == "" & $cast_2 == "" ){
+
+				$casts = "Non disponibile";
+			}
+
 			$message1 = "<b>Nome Serie:</b>%0A".$title_serie."%0A %0A"."<b>Genere:</b>%0A".$genere."%0A %0A"."<b>Data uscita 1° Episodio:</b>%0A".$date_serie."%0A %0A"."<b>Durata Media Episodio:</b>%0A".$durata." min %0A %0A".$rating."<b>Produttore:</b>%0A".$produttore."%0A %0A"."<b>Cast:</b>%0A".$casts;
 			$message2 = $trama;
 
