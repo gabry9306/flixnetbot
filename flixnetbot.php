729bb83795a6e4683f27d2e0f21e20c9942ad8d7
gabry9306 <gabrieledellaria34@gmail.com>
2017-05-29 17:16:11 +0200

    Add files via upload

--- flixnetbot.php
+++ flixnetbot.php
@@ -254,7 +254,7 @@ function GetSerie($chatId,$title)
 				$casts = "Non disponibile";
 			}
 
-			$message1 = "<b>Nome Serie:</b>%0A".$title_film."%0A %0A"."<b>Genere:</b>%0A".$genere."%0A %0A"."<b>Data uscita 1° Episodio:</b>%0A".$date_serie."%0A %0A"."<b>Durata Media Episodio:</b>%0A".$durata." min %0A %0A".$rating."<b>Produttore:</b>%0A".$produttore."%0A %0A"."<b>Cast:</b>%0A".$casts;
+			$message1 = "<b>Nome Serie:</b>%0A".$title_film."%0A %0A"."<b>Genere:</b>%0A".$genere."%0A %0A"."<b>Data uscita 1° Episodio:</b>%0A".$date_serie."%0A %0A"."<b>Durata Media Episodio:</b>%0A".$durata." min %0A %0A".$rating."<b>Produttore:</b>%0A".$produttore."%0A %0A"/*."<b>Cast:</b>%0A".$casts*/;
 			$message2 = $trama;
 
 			if ( $trailer == "www.youtube.com/watch?v=/"){
