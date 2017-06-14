41fccb55657b22f7fec4b7374e918a75927bef22
gabry9306 <gabrieledellaria34@gmail.com>
2017-06-14 15:49:00 +0200

    Add files via upload

--- flixnetbot.php
+++ flixnetbot.php
@@ -61,7 +61,7 @@ function Pagamento($chatId)
 {
 	$pay ='pay';
 	$tastiera = '&prices=[{"label":"Acquisto BluRay Lo Squalo","amount":100}]';
-	$url = $GLOBALS[website].'/sendInvoice?chat_id='.$chatId.'&title=Lo Squalo&description=Blu Ray Lo Squalo&payload=1&provider_token=284685063:TEST:YzFiMTRiOTUwNjY1&start_parameter=pay&currency=EUR&photo_url=https://www.taxidrivers.it/wp-content/uploads/2015/06/Lo-Squalo.jpg'.$tastiera;
+	$url = $GLOBALS[website].'/sendInvoice?chat_id='.$chatId.'&title=Lo Squalo&description=Blu Ray Lo Squalo&payload=1&provider_token=284685063:TEST:YzFiMTRiOTUwNjY1&start_parameter=pay&currency=EUR&photo_url=https://www.taxidrivers.it/wp-content/uploads/2015/06/Lo-Squalo.jpg&photo_size=s'.$tastiera;
 	file_get_contents($url);
 
 	//sendMessage($chatId,"Pagamento!");
