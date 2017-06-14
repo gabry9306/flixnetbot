9e6e516a428c69254336d6bb141d81d4b103d599
gabry9306 <gabrieledellaria34@gmail.com>
2017-06-14 15:26:14 +0200

    Add files via upload

--- flixnetbot.php
+++ flixnetbot.php
@@ -60,8 +60,8 @@ function TastieraMenuPrincipale($chatId,$message)
 function Pagamento($chatId)
 {
 	$pay ='pay';
-	$tastiera = '&prices=[{"label":"label_prova","amount":145}]';
-	$url = $GLOBALS[website].'/sendInvoice?chat_id='.$chatId.'&title=ProvaProdotto&description=DESCRIPTION&payload=1&provider_token=350862534:LIVE:MzU1MThiNjA1YzQw&start_parameter=pay&currency=EUR'.$tastiera;
+	$tastiera = '&prices=[{"label":"label_prova","amount":100}]';
+	$url = $GLOBALS[website].'/sendInvoice?chat_id='.$chatId.'&title=Lo Squalo&description=Blu Ray Lo Squalo&payload=1&provider_token=350862534:LIVE:MzU1MThiNjA1YzQw&start_parameter=pay&currency=EUR'.$tastiera;
 	file_get_contents($url);
 
 	sendMessage($chatId,"Pagamento!");
