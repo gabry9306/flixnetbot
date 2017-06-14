a9bdb013832436d2287f9af8d561711ec637793e
gabry9306 <gabrieledellaria34@gmail.com>
2017-06-14 15:19:16 +0200

    Add files via upload

--- flixnetbot.php
+++ flixnetbot.php
@@ -59,7 +59,7 @@ function TastieraMenuPrincipale($chatId,$message)
 
 function Pagamento($chatId)
 {
-	pay = TRUE;
+	'pay' = TRUE;
 	$tastiera = '&prices=[{"label":"label_prova","amount":"145"}]';
 	$url = $GLOBALS[website].'/sendInvoice?chat_id='.$chatId.'&title=ProvaProdotto&description=DESCRIPTION&payload=1&provider_token=350862534:LIVE:MzU1MThiNjA1YzQw&start_parameter=pay&currency=EUR'.$tastiera;
 	file_get_contents($url);
